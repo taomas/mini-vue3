e192a01b772a5621a0e6d3e28640b6498a9a5689
taomasliu <taomasliu@tencent.com>
2022-10-26 17:38:37 +0800

    feat: 新增pnpm&jest

---
+++ packages/pkg1/index.ts
@@ -0,0 +1,9 @@
+/**
+ * 测试模块1的方法add
+ * @param a 
+ * @param b 
+ * @returns 
+ */
+ export function add(a: number, b: number) {
+  return a + b;
+}
---
+++ packages/pkg2/index.ts
@@ -0,0 +1,9 @@
+/**
+ * 测试模块2的方法add
+ * @param a 
+ * @param b 
+ * @returns 
+ */
+ export function add1(a: number, b: number) {
+  return a + b;
+}
